3ea41f93820a2a7ee72428d74bd61dc2b660d797
Inheo <agch777505@gmail.com>
2022-07-12 10:43:52 +0300

    created copy rotation compoent

---
+++ Assets/Scripts/Stickman/RotationCopyrator.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[RequireComponent(typeof(ConfigurableJoint))]
+public class RotationCopyrator : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+
+    private ConfigurableJoint _configurableJoint;
+    private Quaternion _startRotation;
+
+    private void Awake()
+    {
+        _configurableJoint = GetComponent<ConfigurableJoint>();
+
+        _startRotation = transform.localRotation;
+    }
+
+    private void FixedUpdate()
+    {
+        _configurableJoint.targetRotation = Quaternion.Inverse(_target.localRotation) * _startRotation;
+    }
+}
