641e7872be3813bf679620e6d779f2faa9f2fc01
Inheo <agch777505@gmail.com>
2022-07-25 12:27:21 +0300

    added physci rotatation component

---
+++ Assets/Scripts/Unit/PhysicRotationTo.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[RequireComponent(typeof(ConfigurableJoint))]
+public class PhysicRotationTo : MonoBehaviour
+{
+    [SerializeField] private Transform _to;
+
+    private ConfigurableJoint _pelvisJoint;
+
+    private Quaternion _startRotation;
+
+    private void Awake()
+    {
+        _startRotation = transform.rotation;
+        _pelvisJoint = GetComponent<ConfigurableJoint>();
+    }
+    private void FixedUpdate()
+    {
+        Vector3 direction = _to.position - transform.position;
+        direction.y = 0;
+        _pelvisJoint.targetRotation = Quaternion.Inverse(Quaternion.LookRotation(direction)) * _startRotation;
+    }
+}
