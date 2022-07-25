bb9eb0541356aa23718d0f89dfd6b54a5a08134f
Inheo <agch777505@gmail.com>
2022-07-25 12:28:08 +0300

    split of movment logic

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,12 +5,15 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private JoystickInput _input;
+    [SerializeField] private Animator _animator;
 
     private Rigidbody _rigidbody;
+    private AnimatorTransition _animatorTransition;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _animatorTransition = new AnimatorTransition(_animator);
     }
 
     private void Start()
@@ -22,10 +25,20 @@ public class PlayerMovement : MonoBehaviour
     private void Stop()
     {
         _rigidbody.velocity = Vector3.zero;
+        _animatorTransition.Idle();
     }
 
     private void Move(Vector2 direction)
     {
         _rigidbody.velocity = Vector3.right * direction * _speed;
+
+        if (direction.x > 0)
+        {
+            _animatorTransition.RunForward();
+        }
+        else if (direction.x < 0)
+        {
+            _animatorTransition.RunBack();
+        }
     }
 }
