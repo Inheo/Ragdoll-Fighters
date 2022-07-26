db0f9afe6317271e06c6a3f4fffb90551bff71c1
Inheo <agch777505@gmail.com>
2022-07-26 14:27:10 +0300

    added player component

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+[RequireComponent(typeof(PlayerAttack))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+
+    private PlayerMovement _movement;
+    private PlayerAttack _attack;
+
+    private AnimatorTransition _animatorTransition;
+
+    private void Awake()
+    {
+        _movement = GetComponent<PlayerMovement>();
+        _attack = GetComponent<PlayerAttack>();
+
+        _animatorTransition = new AnimatorTransition(_animator);
+    }
+
+    private void Start()
+    {
+        _movement.Initialize(_animatorTransition);
+        _attack.Initialize(_animatorTransition);
+    }
+}
