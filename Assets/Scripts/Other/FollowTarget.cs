d45ea861cf9f7bb2a836bc511d366a4b26d3f777
Inheo <agch777505@gmail.com>
2022-07-30 19:58:12 +0300

    created component for follow target

---
+++ Assets/Scripts/Other/FollowTarget.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    [System.Flags]
+    public enum FollowAxis
+    {
+        X = 1,
+        Y = 2,
+        Z = 4
+    }
+
+    [SerializeField] private Transform _target;
+    [SerializeField] private FollowAxis _followAxis = FollowAxis.X | FollowAxis.Y | FollowAxis.Z;
+
+    private Vector3 _offset;
+
+    private void Start()
+    {
+        _offset = _target.position - transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = _target.position - _offset;
+        newPosition.x = (_followAxis & FollowAxis.X) == FollowAxis.X ? newPosition.x : transform.position.x;
+        newPosition.y = (_followAxis & FollowAxis.Y) == FollowAxis.Y ? newPosition.y : transform.position.y;
+        newPosition.z = (_followAxis & FollowAxis.Z) == FollowAxis.Z ? newPosition.z : transform.position.z;
+
+        transform.position = newPosition;
+    }
+}
