30f26fd2e14d946a3f30bf92e41e80766a5c9de1
Inheo <agch777505@gmail.com>
2022-07-31 14:19:30 +0300

    added equal owner

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -4,9 +4,16 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] private float _damage = 1f;
 
+    private Unit _owner;
+
     private bool _isActive = false;
 
     public float Damage => _damage;
+
+    public void Initialize(Unit owner)
+    {
+        _owner = owner;
+    }
 
     public void Active()
     {
@@ -20,7 +27,7 @@ public class Weapon : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        if (_isActive == true && other.rigidbody != null && other.rigidbody.TryGetComponent(out ITakeDamage takeDamage))
+        if (_isActive == true && other.rigidbody != null && other.rigidbody.TryGetComponent(out ITakeDamage takeDamage) && _owner != takeDamage.Owner)
         {
             Deactive();
             takeDamage.TakeDamage(_damage);
