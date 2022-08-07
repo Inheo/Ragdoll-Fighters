1e76037363c677642412905e9bea7337cc91a42d
Inheo <agch777505@gmail.com>
2022-08-07 10:58:24 +0300

    created unit movement component

---
+++ Assets/Scripts/Unit/UnitMovement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class UnitMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private Rigidbody _rigidbody;
+    private AnimatorTransition _animatorTransition;
+
+    public void Initialize(AnimatorTransition animatorTransition)
+    {
+        _animatorTransition = animatorTransition;
+    }
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    protected void Move(Vector2 direction)
+    {
+        _rigidbody.velocity = Vector3.right * direction * _speed;
+
+        if (direction.x > 0)
+        {
+            _animatorTransition.RunForward();
+        }
+        else if (direction.x < 0)
+        {
+            _animatorTransition.RunBack();
+        }
+    }
+
+    protected void Stop()
+    {
+        _rigidbody.velocity = Vector3.zero;
+        _animatorTransition.Idle();
+    }
+}
