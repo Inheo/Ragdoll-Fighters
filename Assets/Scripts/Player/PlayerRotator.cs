e5f8b3b1e214861cf4ca6b863eee95f2bf62435d
Inheo <agch777505@gmail.com>
2022-08-07 11:04:48 +0300

    implemented player rotator script on unit rotator

--- Assets/Scripts/Player/PlayerRotator.cs
+++ Assets/Scripts/Player/PlayerRotator.cs
@@ -1,13 +1,1 @@
-using UnityEngine;
-
-public class PlayerRotator : MonoBehaviour
-{
-    [SerializeField] private Transform _target;
-
-    private void Update()
-    {
-        Vector3 direction = _target.position - transform.position;
-        direction.y = 0;
-        transform.rotation = Quaternion.LookRotation(direction);
-    }
-}
+public class PlayerRotator : UnitRotator{}
