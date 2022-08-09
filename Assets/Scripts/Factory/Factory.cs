2643d8502ca324ecfe459cd10f596f179e942bba
Inheo <agch777505@gmail.com>
2022-08-09 20:52:54 +0300

    update factory

--- Assets/Scripts/Factory/Factory.cs
+++ Assets/Scripts/Factory/Factory.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+using System;
 
 public abstract class Factory<T> : MonoBehaviour where T : MonoBehaviour
 {
-    [SerializeField] private T _prefab;
+    [SerializeField] private GameObject _prefab;
     [SerializeField] private Transform _spawnPoint;
     [SerializeField] private Transform _parent;
 
     public virtual T Create()
     {
-        return Instantiate(_prefab, _spawnPoint.position, Quaternion.identity, _parent);
+        T result = Instantiate(_prefab, _spawnPoint.position, Quaternion.identity, _parent).GetComponentInChildren<T>();
+
+        if(result == null)
+            new ArgumentException("Prefab have not nedded type: " + typeof(T));
+
+        return result;
     }
 }
