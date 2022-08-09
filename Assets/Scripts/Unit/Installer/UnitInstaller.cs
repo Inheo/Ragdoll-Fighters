fb1ce8075f8b73ae38cfe59435079d1f14d1d4b7
Inheo <agch777505@gmail.com>
2022-08-09 20:54:09 +0300

    added Unit owner bind in unit installer

--- Assets/Scripts/Unit/Installer/UnitInstaller.cs
+++ Assets/Scripts/Unit/Installer/UnitInstaller.cs
@@ -4,6 +4,7 @@ using Zenject;
 public class UnitInstaller : MonoInstaller
 {
     [SerializeField] private Animator _animator;
+    [SerializeField] private Unit _owner;
     [SerializeField] private HealthSettings _health;
     [SerializeField] private AttackSettings _attack;
     [SerializeField] private MovementSettings _movement;
@@ -11,11 +12,12 @@ public class UnitInstaller : MonoInstaller
     public override void InstallBindings()
     {
         Container.Bind<Animator>().FromInstance(_animator).AsSingle();
+        Container.Bind<Unit>().FromInstance(_owner).AsSingle();
+        Container.Bind<AnimatorTransition>().AsSingle();
 
         Container.Bind<HealthSettings>().FromInstance(_health).AsSingle();
         Container.Bind<AttackSettings>().FromInstance(_attack).AsSingle();
         Container.Bind<MovementSettings>().FromInstance(_movement).AsSingle();
 
-        Container.Bind<AnimatorTransition>().FromNew().AsSingle();
     }
 }
