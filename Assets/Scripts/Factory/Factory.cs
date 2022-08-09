9266cdbc77859d14d8ad8a6b66ca1d34912a0d22
Inheo <agch777505@gmail.com>
2022-08-09 21:26:09 +0300

    change create unit for factory

--- Assets/Scripts/Factory/Factory.cs
+++ Assets/Scripts/Factory/Factory.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System;
+using Zenject;
 
 public abstract class Factory<T> : MonoBehaviour where T : MonoBehaviour
 {
@@ -7,9 +8,11 @@ public abstract class Factory<T> : MonoBehaviour where T : MonoBehaviour
     [SerializeField] private Transform _spawnPoint;
     [SerializeField] private Transform _parent;
 
+    [Inject] private DiContainer _diContainer;
+
     public virtual T Create()
     {
-        T result = Instantiate(_prefab, _spawnPoint.position, Quaternion.identity, _parent).GetComponentInChildren<T>();
+        T result = _diContainer.InstantiatePrefab(_prefab, _spawnPoint.position, Quaternion.identity, _parent).GetComponentInChildren<T>();
 
         if(result == null)
             new ArgumentException("Prefab have not nedded type: " + typeof(T));
