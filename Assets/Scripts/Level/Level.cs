b08ae15a2b313016b663a55e8deda09693f74d52
Inheo <agch777505@gmail.com>
2022-08-10 12:49:10 +0300

    added get factories and create units

--- Assets/Scripts/Level/Level.cs
+++ Assets/Scripts/Level/Level.cs
@@ -1,15 +1,26 @@
 using System.Linq;
 using UnityEngine;
 using Scripts.Data;
+using Zenject;
 
 public class Level : MonoBehaviour
 {
+    private PlayerFactory _playerFactory;
+    private EnemyFactory _enemyFactory;
+
     public event System.Action OnLevelFinish;
     public event System.Action OnLevelFail;
 
     public bool IsLevelEnd { get; private set; }
 
     public static Level Instance { get; private set; }
+
+    [Inject]
+    public void Construct(PlayerFactory playerFactory, EnemyFactory enemyFactory)
+    {
+        _playerFactory = playerFactory;
+        _enemyFactory = enemyFactory;
+    }
 
     private void Awake()
     {
@@ -20,6 +31,12 @@ public class Level : MonoBehaviour
     {
         IsLevelEnd = false;
         SubscribeEvents();
+
+        Player player = _playerFactory.Create();
+        Enemy enemy = _enemyFactory.Create();
+
+        player.Initialize();
+        enemy.Initialize();
     }
 
     private void OnDestroy()
