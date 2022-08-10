c83523ef92ff2a870de8819fe768a7512b63cef5
Inheo <agch777505@gmail.com>
2022-08-10 12:48:41 +0300

    update Level installer

--- Assets/Scripts/Installers/LevelInstaller.cs
+++ Assets/Scripts/Installers/LevelInstaller.cs
@@ -4,9 +4,17 @@ using Zenject;
 public class LevelInstaller : MonoInstaller
 {
     [SerializeField] private Level _level;
+    [SerializeField] private JoystickInput _input;
+    [SerializeField] private PlayerFactory _playerFactory;
+    [SerializeField] private EnemyFactory _enemyFactory;
 
     public override void InstallBindings()
     {
         Container.Bind<Level>().FromInstance(_level).AsSingle();
+        
+        Container.Bind<JoystickInput>().FromInstance(_input).AsSingle();
+
+        Container.Bind<PlayerFactory>().FromInstance(_playerFactory).AsSingle();
+        Container.Bind<EnemyFactory>().FromInstance(_enemyFactory).AsSingle();
     }
 }
