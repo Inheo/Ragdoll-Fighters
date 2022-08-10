32d7ff016997bf133fe5f756da13ce4a7563ca19
Inheo <agch777505@gmail.com>
2022-08-10 12:47:46 +0300

    added construcror for player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
+using Zenject;
 
 public class PlayerMovement : UnitMovement
 {
     [SerializeField] private JoystickInput _input;
+
+    [Inject]
+    public void Construct(JoystickInput input)
+    {
+        _input = input;
+    }
 
     private void Start()
     {
@@ -13,6 +20,6 @@ public class PlayerMovement : UnitMovement
     private void OnDestroy()
     {
         _input.OnActive -= Move;
-        _input.OnStop -= Stop; 
+        _input.OnStop -= Stop;
     }
 }
