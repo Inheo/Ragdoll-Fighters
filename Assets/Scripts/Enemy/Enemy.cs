9e5a0753b684eb46842ef5d13a573e07670daf33
Inheo <agch777505@gmail.com>
2022-08-10 12:48:17 +0300

    override Initialize method from basic class

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,4 +2,11 @@ using UnityEngine;
 
 [RequireComponent(typeof(EnemyMovement))]
 [RequireComponent(typeof(EnemyAttack))]
-public class Enemy : Unit{}
+public class Enemy : Unit
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+        FindTarget<Player>();
+    }
+}
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,4 +4,9 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerAttack))]
 public class Player : Unit
 {
+    public override void Initialize()
+    {
+        base.Initialize();
+        FindTarget<Enemy>();
+    }
 }
