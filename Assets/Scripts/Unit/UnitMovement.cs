511145cf3d8919cbb200cafbdd52198f128b94e6
Inheo <agch777505@gmail.com>
2022-08-13 18:37:57 +0300

    added moevement condition

--- Assets/Scripts/Unit/UnitMovement.cs
+++ Assets/Scripts/Unit/UnitMovement.cs
@@ -8,12 +8,14 @@ public class UnitMovement : MonoBehaviour
 
     private Rigidbody _rigidbody;
     private AnimatorTransition _animatorTransition;
+    private ICanActionable _canActionable;
 
     [Inject]
-    public void Construct(MovementSettings settings, AnimatorTransition animatorTransition)
+    public void Construct(MovementSettings settings, AnimatorTransition animatorTransition, ICanActionable canActionable)
     {
         _settings = settings;
         _animatorTransition = animatorTransition;
+        _canActionable = canActionable;
     }
 
     private void Awake()
@@ -24,10 +26,16 @@ public class UnitMovement : MonoBehaviour
     protected virtual void Update()
     {
         transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, 0);
+        
+        if(_canActionable.IsCanAction() == false)
+            Stop();
     }
 
     protected void Move(Vector2 direction)
     {
+        if (_canActionable.IsCanAction() == false)
+            return;
+
         _rigidbody.velocity = Vector3.right * direction * _settings.Speed;
 
         if (direction.x > 0)
