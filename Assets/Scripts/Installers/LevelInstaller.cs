14de21641bbae6f474fcdabe39b34b2bde4a0a1d
Inheo <agch777505@gmail.com>
2022-08-14 18:50:41 +0300

    pdate level installer

--- Assets/Scripts/Installers/LevelInstaller.cs
+++ Assets/Scripts/Installers/LevelInstaller.cs
@@ -4,17 +4,33 @@ using Zenject;
 public class LevelInstaller : MonoInstaller
 {
     [SerializeField] private Level _level;
+    [SerializeField] private LevelStartButton _levelStartButton;
     [SerializeField] private JoystickInput _input;
     [SerializeField] private PlayerFactory _playerFactory;
     [SerializeField] private EnemyFactory _enemyFactory;
 
     public override void InstallBindings()
     {
-        Container.Bind<Level>().FromInstance(_level).AsSingle();
-        
-        Container.Bind<JoystickInput>().FromInstance(_input).AsSingle();
+        LevelBind();
+        JoystickBind();
 
+        FactoriesBind();
+    }
+
+    private void FactoriesBind()
+    {
         Container.Bind<PlayerFactory>().FromInstance(_playerFactory).AsSingle();
         Container.Bind<EnemyFactory>().FromInstance(_enemyFactory).AsSingle();
+    }
+
+    private void LevelBind()
+    {
+        Container.Bind<Level>().FromInstance(_level).AsSingle();
+        Container.Bind<LevelStartButton>().FromInstance(_levelStartButton).AsSingle();
+    }
+
+    private void JoystickBind()
+    {
+        Container.Bind<JoystickInput>().FromInstance(_input).AsSingle();
     }
 }
