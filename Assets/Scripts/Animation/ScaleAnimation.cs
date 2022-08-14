27a1ab883f7e03dbf2b81761f6ab57b1c341c720
Inheo <agch777505@gmail.com>
2022-08-14 19:16:54 +0300

    created animation script

---
+++ Assets/Scripts/Animation/ScaleAnimation.cs
@@ -0,0 +1,17 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class ScaleAnimation : MonoBehaviour
+{
+    [SerializeField] private float _duration = 0.3f;
+    [SerializeField] private float _delay = 0.3f;
+    [SerializeField] private Vector3 _startScaleMultiply;
+    private void Start()
+    {
+        Vector3 newScale = new Vector3(_startScaleMultiply.x * transform.lossyScale.x, _startScaleMultiply.y * transform.lossyScale.y, _startScaleMultiply.z * transform.lossyScale.z);
+
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(transform.DOScale(newScale, _duration).SetDelay(_delay));
+        sequence.SetLoops(-1, LoopType.Yoyo);
+    }
+}
