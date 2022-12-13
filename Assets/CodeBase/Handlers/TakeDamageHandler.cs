98ca670154b5bcf88c9bbe4cbd0d995e76ae435a
Inheo <agch777505@gmail.com>
2022-12-13 20:00:29 +0300

    up take handler

--- Assets/CodeBase/Handlers/TakeDamageHandler.cs
+++ Assets/CodeBase/Handlers/TakeDamageHandler.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class TakeDamageHandler : MonoBehaviour, ITakeDamage
 {
-    private Unit _unit;
+    private UnitHealth _unit;
 
-    public Unit Owner => _unit;
+    public Unit Owner { get; private set; }
 
     private void Start()
     {
-        _unit = GetComponentInParent<Unit>();
+        Owner = GetComponentInParent<Unit>();
+        _unit = GetComponentInParent<UnitHealth>();
     }
 
     public void TakeDamage(float damage)
