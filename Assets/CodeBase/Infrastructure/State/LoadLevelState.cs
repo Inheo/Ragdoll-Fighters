d6baab18e0308cfa962be6d6cbe3ff872ef810e6
Inheo <agch777505@gmail.com>
2022-12-13 20:01:36 +0300

    up load level state

--- Assets/CodeBase/Infrastructure/State/LoadLevelState.cs
+++ Assets/CodeBase/Infrastructure/State/LoadLevelState.cs
@@ -1,9 +1,17 @@
+using System.Threading.Tasks;
+using CodeBase.Infrastructure.Factory;
+using CodeBase.Service.StaticData;
+using CodeBase.StaticData;
+using UnityEngine.SceneManagement;
+
 namespace CodeBase.Infrastructure.States
 {
     public class LoadLevelState : IPayloadedState<string>
     {
         private readonly SceneLoader _sceneLoader;
         private readonly GameStateMachine _stateMachine;
+        private readonly IGameFactory _factory;
+        private readonly IStaticDataService _staticData;
 
         public void Enter(string payload)
         {
@@ -15,9 +23,27 @@ namespace CodeBase.Infrastructure.States
             
         }
 
-        private void Loaded()
+        private async void Loaded()
         {
             _stateMachine.Enter<GameLoopState>();
+            await InitGameWorld();
         }
+
+        private async Task InitGameWorld()
+        {
+            LevelStaticData levelData = LevelStaticData();
+
+            await InitPlayer(levelData);
+            await InitEnemy(levelData);
+        }
+
+        private LevelStaticData LevelStaticData() =>
+            _staticData.ForLevel(SceneManager.GetActiveScene().name);
+
+        private async Task InitEnemy(LevelStaticData levelData) => 
+            await _factory.CreatePlayer(levelData.EnemyData.Position);
+
+        private async Task InitPlayer(LevelStaticData levelData) => 
+            await _factory.CreatePlayer(levelData.PlayerPosition);
     }
 }
