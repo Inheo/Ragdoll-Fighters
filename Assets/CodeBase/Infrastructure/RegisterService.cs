cb60c408d768f9d94fe23f7753768e36091ed637
Inheo <agch777505@gmail.com>
2022-12-25 12:47:27 +0300

    register new service

--- Assets/CodeBase/Infrastructure/RegisterService.cs
+++ Assets/CodeBase/Infrastructure/RegisterService.cs
@@ -3,16 +3,35 @@ using CodeBase.Infrastructure.Factory;
 using CodeBase.Service.PersistentProgress;
 using CodeBase.Service.SaveLoad;
 using CodeBase.Service.StaticData;
+using UnityEngine;
 using Zenject;
 
 public class RegisterService : MonoInstaller
 {
+    [SerializeField] private JoystickInput _input;
+    [SerializeField] private GameObject _levelStartButton;
+
     public override void InstallBindings()
     {
+        BindJoystick();
+        BindLevelStartButton();
+
         Container.Bind<IPersistentProgressService>().To<PersistentProgressService>().AsSingle();
         Container.Bind<ISaveLoadService>().To<SaveLoadService>().AsSingle();
         Container.Bind<IGameFactory>().To<GameFactory>().AsSingle();
         Container.Bind<IStaticDataService>().To<StaticDataService>().AsSingle();
         Container.Bind<IAssetProvider>().To<AssetProvider>().AsSingle();
+    }
+
+    public void BindJoystick()
+    {
+        var input = Container.InstantiatePrefabForComponent<JoystickInput>(_input);
+        Container.Bind<JoystickInput>().FromInstance(input).AsSingle();
+    }
+
+    public void BindLevelStartButton()
+    {
+        var button = Container.InstantiatePrefabForComponent<LevelStartButton>(_levelStartButton);
+        Container.Bind<LevelStartButton>().FromInstance(button).AsSingle();
     }
 }
